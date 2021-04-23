4b38842b20ef2876871b3cb44a1231ff3a15f997
leedc0 <leedc@me.com>
2021-04-22 21:56:22 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    @board = Array.new(9, " ")
+    index = 0
+
+    # !(@board[index].nil? || @board[index] == " ")
+
+    return false if [" ", "", nil].include?(board[index])
+    return true if ["X", "O"].include?(board[index])
+  
+end
